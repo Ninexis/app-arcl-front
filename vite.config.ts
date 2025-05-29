18d7ac29abd0fe33580233c4ed9e22d0c1b78d0c
josef.tourrand <josef.tourrand@epita.fr>
2025-05-29 04:52:04 +0200

    fix: run front on port 80

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    server: {
+        port: 80, // 🎯 port d'écoute
+        host: true // 🔓 écoute sur 0.0.0.0 (important si tu es sur une VM ou un container)
+    },
+    plugins: [react()],
 })
