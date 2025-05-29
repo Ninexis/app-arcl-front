d964d44c2bc7328fbed307fafff9c59d48db2a52
josef.tourrand <josef.tourrand@epita.fr>
2025-05-29 05:17:48 +0200

    fix: reverted changes to vite config: need sudo to run on port 80

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    server: {
-        port: 80, // 🎯 port d'écoute
-        host: true // 🔓 écoute sur 0.0.0.0 (important si tu es sur une VM ou un container)
-    },
     plugins: [react()],
 })
