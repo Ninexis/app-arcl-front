2ab7ef90fdf452bc85dee155328d0762503d55ff
josef.tourrand <josef.tourrand@epita.fr>
2025-06-01 00:52:53 +0200

    fix: allow hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     server: {
         port: 80, // 🎯 port d'écoute
         host: true, // 🔓 écoute sur 0.0.0.0 (important si tu es sur une VM ou un container)
-        allowedHosts: "web-lb-1331050834.eu-north-1.elb.amazonaws.com"
+        allowedHosts: "all",
     },
     plugins: [react()],
 })
