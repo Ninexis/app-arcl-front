3cd8bb7767d63791c9ef8dc4d9784c4ad85a61a1
josef.tourrand <josef.tourrand@epita.fr>
2025-05-31 23:03:58 +0200

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     server: {
         port: 80, // 🎯 port d'écoute
-        host: true // 🔓 écoute sur 0.0.0.0 (important si tu es sur une VM ou un container)
+        host: true, // 🔓 écoute sur 0.0.0.0 (important si tu es sur une VM ou un container)
+        allowedHosts: "web-lb-1331050834.eu-north-1.elb.amazonaws.com"
     },
     plugins: [react()],
 })
