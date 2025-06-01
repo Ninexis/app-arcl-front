b2480a18cd99643b039537047a2a4eac27062270
josef.tourrand <josef.tourrand@epita.fr>
2025-06-01 04:02:55 +0200

    revert config again

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    server: {
-        port: 80, // 🎯 port d'écoute
-        host: true, // 🔓 écoute sur 0.0.0.0 (important si tu es sur une VM ou un container)
-        strictPort: true,
-        hmr: {
-            protocol: 'ws',
-            host: 'localhost',
-        },
-        allowedHosts: "all",
-    },
     plugins: [react()],
 })
